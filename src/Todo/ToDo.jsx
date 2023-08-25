70d38a0f179d3548158bb5d7c9ebb0508ffca546
Rioske-Nakamura <131805559+Rioske-Nakamura@users.noreply.github.com>
2023-08-25 14:29:56 -0400

    pronto

---
+++ src/Todo/ToDo.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function ToDo() {
+    const [atividade, setAtividade] = useState("");
+    const [lista, setLista] = useState({});
+
+    const salvar = (e) =>{
+       e.preventDefault();
+       setLista([...lista,{
+        atividade: atividade
+       }])
+    };
+    
+    return(
+        <div>
+            <Link to="/">home</Link>
+            <h1>Lista de Atividades</h1>
+
+            <form onSubmit={salvar}>
+                <input onChange={(e) => setAtividade(e.target.value)} type="text"></input>
+                <button>ADD</button>
+
+            </form>
+
+            {lista.map((ativ) => 
+             <div>
+            <p>ativ.atividade</p>
+            </div>
+            )}
+        </div>
+    );
+}
