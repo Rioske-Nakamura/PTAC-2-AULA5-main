e06ff0438c1b1c1a04e3fc7e2ea9229d6548d3f1
Rioske-Nakamura <131805559+Rioske-Nakamura@users.noreply.github.com>
2023-10-27 14:27:19 -0400

    yy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from './Home/Home';
 import ToDo from './Todo/ToDo';
+import Detalhe from './detalhe/detalhe';
 import ReaCte from './React/Reacty';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/todo' element={<ToDo />}></Route>
+        <Route path='/detalhe:id' element={<Detalhe />}></Route>
         <Route path='/' element={<Home />}></Route>
         <Route path='/Trabalho' element={<ReaCte />}></Route>
       </Routes>
