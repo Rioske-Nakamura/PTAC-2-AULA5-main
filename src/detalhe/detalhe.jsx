13b2d1418102cbb552c5c57a51a17a0d0afe0dd5
Rioske-Nakamura <rioskenakamura@gmail.com>
2023-11-17 20:37:26 -0400

    aeee

--- src/detalhe/detalhe.jsx
+++ src/detalhe/detalhe.jsx
@@ -1,20 +1,19 @@
 import { object } from "prop-types";
 import { useParams } from "react-router-dom";
+import Card from "../componente/card";
 
 export default function Detalhe() {
     const{id} = useParams();
     const lista= JSON.parse(localStorage.getItem("lista"))
 
     const atividade = lista.filter((objeto) => {
-        if(objeto.id == id){
+        if(objeto.id === parseInt(id, 10)){
             return objeto
         }
         return null
     })
  console.log(atividade[0])
     return(
-        <div>
-            <h1>Ola {id}</h1>
-        </div>
+        <Card atividade={atividade[0]}/>
     );
 }
